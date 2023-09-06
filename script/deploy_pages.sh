e4555f805b5d837f3a2a19c6fb10a94e2f92e41a
zhengyi <admin@mczhengyi.top>
2023-09-06 23:56:03 +0800

    ci: deploy to github/gitee directly

---
+++ script/deploy_pages.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+DEPLOY_TARGET=(${DEPLOY_REPO//;/ })
+DIST_DIR="docs/.vuepress/dist"
+DEPLOY_BRANCH="gh-docs"
+
+if [ -d "$DIST_DIR" ]; then
+    cd "$DIST_DIR"
+elif [ -d "../$DIST_DIR" ]; then
+    cd "../$DIST_DIR"
+else
+    echo "==> not found dist dir, exit"
+    exit 0
+fi
+
+# Add CNAME
+echo "==> Will add CNAME file to git repo"
+echo "==> CNAME: $CNAME"
+echo "$CNAME" > CNAME
+
+# Commit
+git init
+git checkout -b gh-docs
+git add .
+git commit -m "deploy from $CI_COMMIT_SHORT_SHA"
+
+# Deploy to git repo
+for var in ${DEPLOY_TARGET[@]}
+do
+  echo "==> Deploying to git repo: $var, branch $DEPLOY_BRANCH"
+  git push -f -u $var $DEPLOY_BRANCH
+done
